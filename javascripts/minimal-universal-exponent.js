52b5b634f18aba0fdd24213b3bc3527d6d3aa26e
Devraj Mehta <devm33@gmail.com>
2014-04-12 16:01:26 -0400

    similiar to last, error clarity

--- javascripts/minimal-universal-exponent.js
+++ javascripts/minimal-universal-exponent.js
@@ -1,3 +1,30 @@
 function min_unv_exp(num) {
-    return 0; //TODO
+    /* Returns a number representing the minimal universal exponent
+     * of a positive integer num, which is assumed to be valid */
+    var e = 0;
+    //TODO
+    
+    return e;
+}
+
+
+function prime_factorization(n) {
+    /* Returns an array of prime factors (in least sorted order)
+     * where each factor is an object of the form
+     * {'p': 7, 'e': 4}
+     * where p is the prime base and e is the exponent to which that
+     * factor is raised in the factorization of n
+     */
+    var factors = [], e;
+    for(var p = 2; p <= n; p++) {
+        e = 0;
+        while(n % e == 0) {
+            n = n / p;
+            e++;
+        }
+        if(e > 0) {
+            factors.push({'p':p,'e':e});
+        }
+    }
+    return factors;
 }
