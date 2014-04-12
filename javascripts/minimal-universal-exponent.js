49ae68d05c3548758a0f30fc07f63ffb33d65fea
Devraj Mehta <devm33@gmail.com>
2014-04-12 16:23:44 -0400

    implemented exponent function

--- javascripts/minimal-universal-exponent.js
+++ javascripts/minimal-universal-exponent.js
@@ -1,12 +1,27 @@
 function min_unv_exp(num) {
     /* Returns a number representing the minimal universal exponent
      * of a positive integer num, which is assumed to be valid */
-    var e = 0;
-    //TODO
+     
+    var e = 1; /* This will be the returned exponent, note: needs to be
+    one for the first lcm to work */
+
+    /* Iterate through prime factors, computing lcm of each's totient */
+    prime_factorization(num).forEach(function(factor){
+        if(factor.p === 2) {
+            if(factor.e == 2) {
+                e = lcm(e, 2);
+            }
+            else if(factor.e > 2) {
+                e = lcm(e, Math.pow(2, factor.e - 2));
+            }
+        }
+        else {
+            e = lcm(e, Math.pow(factor.p, factor.e - 1) * (factor.p - 1));
+        }
+    });
     
     return e;
 }
-
 
 function prime_factorization(n) {
     /* Returns an array of prime factors (in least sorted order)
@@ -18,7 +33,7 @@ function prime_factorization(n) {
     var factors = [], e;
     for(var p = 2; p <= n; p++) {
         e = 0;
-        while(n % e == 0) {
+        while(n % p == 0) {
             n = n / p;
             e++;
         }
@@ -27,4 +42,24 @@ function prime_factorization(n) {
         }
     }
     return factors;
+}
+
+function gcd(a, b){
+    /* Returns greatest common divisor of two (assumed) positive integers
+     * Uses Euclidean algorithm
+     */
+    var t;
+    while (b != 0){
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+function lcm(a, b){
+    /* Returns least common multiple of two (assumed) positive integers
+     * Uses gcd function above
+     */
+    return (a * b / gcd(a, b));
 }
